89b00199b392056caaa3649eec6f4ecb9f316f6a
Erika Miguel <ms.erikamiguel@gmail.com>
2018-12-04 11:17:02 -0500

    adding service worker

--- js/main.js
+++ js/main.js
@@ -12,6 +12,17 @@ document.addEventListener('DOMContentLoaded', (event) => {
   fetchCuisines();
 });
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('../sw.js')
+  .then(function(reg) {
+    // registration worked
+    console.log('Registration succeeded. Scope is ' + reg.scope);
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
+}
+
 /**
  * Fetch all neighborhoods and set their HTML.
  */
